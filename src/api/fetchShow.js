59577c46b8977100379eee6f9294a06a1dd27c9b
Jason <47641725+Jfadelli@users.noreply.github.com>
2020-07-21 13:26:04 -0700

    'mvp'

---
+++ src/api/fetchShow.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export const fetchShow = async () => {
+    return await axios
+        .get("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
+        .then(res => { return res.data })
+        .catch(err => { throw err })
+};
